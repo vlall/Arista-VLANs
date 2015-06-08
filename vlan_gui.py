1fd789f18f19ca2891a356f9b1744c83e68d3a43
Vishal <vlall@users.noreply.github.com>
2015-06-08 12:49:43 -0400

    Update vlan_gui.py

--- vlan_gui.py
+++ vlan_gui.py
@@ -1,18 +1,61 @@
+#!/usr/bin/python
+
 from Tkinter import *
-root = Tk()
 
-root.title("VLAN creation GUI")
-root.geometry("200x100")
+class AristaTool(Frame):
+  
+    def __init__(self, parent):
+        Frame.__init__(self, parent)   
+        self.parent = parent        
+        self.initUI()
+        
+    def initUI(self):
+        self.parent.title("Arista Tool")
+        self.pack(fill=BOTH, expand=1)
+        self.var = IntVar()
+        status = 0
 
-app = Frame(root)
-app.grid()
+#Show Vlans
+#Scroll through Vlans
+#Modify Vlans
 
-#Variables
-status = 0
-vlanswitch = vlan()
+        labelButton1 = Label(self, text='Current VLANs')
+        listBox = Listbox(self)
+        vlanList= ['VLAN001', 'VLAN002', 'VLAN003', 'VLAN004']
+        for i in range(len(vlanList)):
+            listBox.insert(i, vlanList[i])
 
-on = Radiobutton(app, text='On', variable=status, value=1)
-on.grid()
-off = Radiobutton(app, text='Off', variable=status, value=0)
-off.grid()
-root.mainloop()
+        listBoxBtn = LabelFrame(self, text="Edit" )
+        listBoxBtn.pack(padx=10, pady=10)
+        w = Button(listBoxBtn, text='Check Status')
+        w.pack()
+        add = Button(listBoxBtn, text='Add')
+        add.pack()
+        remove = Button(listBoxBtn, text='Remove')
+        remove.pack()
+
+       # listBoxBtn = Button(self, text='Check Status')
+        createButton = Button(self, text="Confirm")
+
+#Create Vlans
+        #onButton.place(x=50, y=20)
+        #offButton.place(x=50, y=50)
+
+        labelButton1.grid(row=1, column=2)
+        listBox.grid(row=2, column=2)
+        listBoxBtn.grid(row=2, column=3)
+
+    def onClick(self):
+        if self.var.get() == 1:
+            self.master.title("VLAN Too ")
+        else:
+            self.master.title("")
+
+def main():
+    root = Tk()
+    root.geometry("550x350+300+300")
+    app = AristaTool(root)
+    root.mainloop()  
+
+if __name__ == '__main__':
+    main()  
